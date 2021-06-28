e176b1e9c9a530e5e1e613ce5b8359335d7e886d
ThiagoMax <thiagomaximiliano21@gmail.com>
2021-06-28 13:35:50 -0300

    Firebase configurado, chat_screen e text_composer

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+
+  DocumentSnapshot snapshot = await Firestore.instance
+      .collection("mensagens")
+      .document("0APHBU1h7zm17pPncwII")
+      .get();
+  print(snapshot.data);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.blue)),
+      home: ChatScreen(),
+    );
+  }
+}
