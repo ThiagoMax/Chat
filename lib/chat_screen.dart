5a178c393be4174c4f4b7111f9028d1b5f60a269
ThiagoMax <thiagomaximiliano21@gmail.com>
2021-07-05 13:15:28 -0300

    LinearProgressIndicator, orderBy das mensagens e alinhando mensagens dos usuarios

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -8,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
+import 'ChatMessage.dart';
+
 class ChatScreen extends StatefulWidget {
   //const ChatScreen({ Key? key }) : super(key: key);
 
@@ -19,12 +21,15 @@ class _ChatScreenState extends State<ChatScreen> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   FirebaseUser _currentUser;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
     FirebaseAuth.instance.onAuthStateChanged.listen((user) {
-      _currentUser = user;
+      setState(() {
+        _currentUser = user;
+      });
     });
   }
 
@@ -61,7 +66,8 @@ class _ChatScreenState extends State<ChatScreen> {
     Map<String, dynamic> data = {
       "uid": user.uid,
       "senderName": user.displayName,
-      "senderPhotoUrl": user.photoUrl
+      "senderPhotoUrl": user.photoUrl,
+      "time": Timestamp.now()
     };
 
     if (imgFile != null) {
@@ -71,9 +77,17 @@ class _ChatScreenState extends State<ChatScreen> {
           .child(DateTime.now().millisecondsSinceEpoch.toString())
           .putFile(file);
 
+      setState(() {
+        _isLoading = true;
+      });
+
       StorageTaskSnapshot taskSnapshot = await task.onComplete;
       String url = await taskSnapshot.ref.getDownloadURL();
       data["imgUrl"] = url;
+
+      setState(() {
+        _isLoading = false;
+      });
     }
     if (text != null) data["text"] = text;
 
@@ -85,14 +99,33 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        title: Text("Olá"),
+        title: Text(_currentUser != null
+            ? "Olá ${_currentUser.displayName}"
+            : "Chat App"),
+        centerTitle: true,
         elevation: 0,
+        actions: <Widget>[
+          _currentUser != null
+              ? IconButton(
+                  icon: Icon(Icons.exit_to_app),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    googleSignIn.signOut();
+                    _scaffoldKey.currentState.showSnackBar(SnackBar(
+                      content: Text("Você saiu com sucesso!"),
+                    ));
+                  })
+              : Container()
+        ],
       ),
       body: Column(
         children: <Widget>[
           Expanded(
             child: StreamBuilder(
-                stream: Firestore.instance.collection("messages").snapshots(),
+                stream: Firestore.instance
+                    .collection("messages")
+                    .orderBy("time", descending: true)
+                    .snapshots(),
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.none:
@@ -104,14 +137,17 @@ class _ChatScreenState extends State<ChatScreen> {
                           snapshot.data.documents;
                       return ListView.builder(
                           itemBuilder: (context, index) {
-                            return ListTile(
-                                title: Text(documents[index].data["text"]));
+                            return ChatMessage(
+                                documents[index].data,
+                                documents[index].data["uid"] ==
+                                    _currentUser?.uid);
                           },
                           itemCount: documents.length,
                           reverse: true);
                   }
                 }),
           ),
+          _isLoading ? LinearProgressIndicator() : Container(),
           TextComposer(_sendMessage)
         ],
       ),
